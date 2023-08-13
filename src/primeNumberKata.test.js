8e9ea16b14b42777ad1e928e7d982c9a489c008e
michael-chung-mc <chung.michael.mc@gmail.com>
2023-08-13 05:36:31 -0400

    INIT prime number kata test list

--- src/primeNumberKata.test.js
+++ src/primeNumberKata.test.js
@@ -5,3 +5,15 @@ describe("canary", ()=> {
         expect(true).toBe(true);
     })
 })
+
+describe("prime number function should", ()=> {
+    it.todo("return none for 1");
+    it.todo("return 2 for 2");
+    it.todo("return 3 for 3");
+    it.todo("return 2, 2 for 4");
+    it.todo("return 5 for 5");
+    it.todo("return 2,3 for 6");
+    it.todo("return 7 for 7");
+    it.todo("return 2,2,2 for 8");
+    it.todo("return 3,3 for 9");
+})
