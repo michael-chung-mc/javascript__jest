0af82b96b73b196f3c1b0891026360da318f03f6
michael-chung-mc <chung.michael.mc@gmail.com>
2023-08-13 05:41:52 -0400

    SET prime number return [3] for 3

--- src/primeNumberKata.test.js
+++ src/primeNumberKata.test.js
@@ -10,10 +10,12 @@ describe("prime number function should", ()=> {
     it("return none for 1", ()=>{
         expect(primeNumber(1)).toEqual([]);
     });
-    it("return 2 for 2", ()=>{
+    it("return [2] for 2", ()=>{
         expect(primeNumber(2)).toEqual([2]);
     });
-    it.todo("return 3 for 3");
+    it("return 3 for 3", ()=>{
+        expect(primeNumber(3)).toEqual([3]);
+    });
     it.todo("return 2, 2 for 4");
     it.todo("return 5 for 5");
     it.todo("return 2,3 for 6");
