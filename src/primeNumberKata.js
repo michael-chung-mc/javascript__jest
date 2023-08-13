67a03fe282bd12c0ddab371785201ea5e10dc8ac
michael-chung-mc <chung.michael.mc@gmail.com>
2023-08-13 05:57:56 -0400

    SET prime number return [3,3] for 9

--- src/primeNumberKata.js
+++ src/primeNumberKata.js
@@ -1,9 +1,18 @@
 function findPrimeNumbers(number) {
     let factors = [];
     if (number > 1) {
+<<<<<<< HEAD
         while (number%2 === 0) {
             factors.push(2);
             number /= 2;
+=======
+        for (let divisor = 2; number > 1; divisor += 1) {
+            while (number%divisor===0)
+            {
+                factors.push(divisor);
+                number/=divisor;
+            }
+>>>>>>> aa5068c (SET prime number return [3,3] for 9)
         }
         if (number > 1) factors.push(number);
     }
