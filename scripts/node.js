a3d06dd003182e32128265c14f9344f378f39d83
michael-chung-mc <chung.michael.mc@gmail.com>
2023-08-25 00:05:21 -0400

    SET node factory

---
+++ scripts/node.js
@@ -0,0 +1,9 @@
+function nodeFactory (value = 0, next = null) {
+    const node = {
+        value : value,
+        next : next,
+    };
+    return node;
+}
+
+module.exports = nodeFactory;
--- scripts/node.test.js
+++ scripts/node.test.js
@@ -1,7 +1,30 @@
-const node = require('./node.js');
+const nodeFactory = require('./node.js');
 
 describe('canary', ()=> {
     it('test testing environment', ()=>{
         expect(true).toBe(true);
+    });
+});
+
+describe('node', ()=> {
+    let head = nodeFactory();
+    let next = nodeFactory(2);
+    let tail = nodeFactory(3);
+    it('handles empty input', ()=>{
+        expect(head).toBeDefined();
+    });
+    head.value = 1;
+    it('can add next node', ()=>{
+        head.next = next;
+        expect(head.next).toBeDefined();
+        expect(head.next).toStrictEqual(next);
+        head.next.next = tail;
+        expect(head.next.next).toBeDefined();
+        expect(head.next.next).toStrictEqual(tail);
+    });
+    it('can delete node', ()=>{
+        head.next = head.next.next;
+        expect(head.next).toBeDefined();
+        expect(head.next).toStrictEqual(tail);
     });
 });
